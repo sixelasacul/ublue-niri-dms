358bf83e53251362dbdf5f76bab6e37dffca871c
sixelasacul <pro@sixe.la>
2026-08-02 16:17:31 +0200

    trying new things

--- build_files/build.sh
+++ build_files/build.sh
@@ -13,10 +13,12 @@ dnf5 install niri dms dgop danksearch dms-greeter -y
 # down to this point it works. no idea why but the systemd stuff aren't set.
 # worst case that could be done by the user
 
-systemctl --user enable dms
-systemctl --user add-wants niri.service dms
-systemctl --user disable gdm lightdm sddm
-systemctl --user enable greetd
+ln -s /usr/lib/systemd/system/shell-setup.service /usr/lib/systemd/system/multi-user.target.wants/shell-setup.service
+
+# systemctl --user enable dms
+# systemctl --user add-wants niri.service dms
+# systemctl --user disable gdm lightdm sddm
+# systemctl --user enable greetd
 
 # note: could use this build to install all my stuff across systems
 # like zed, zen, bitwarden, localsend, via, etc.
