15d4862ab1bbb66179a7d5c976c84d2a5c36437e
sixelasacul <pro@sixe.la>
2026-08-02 12:07:03 +0200

    either remove user flag or just remove systemctl from here, we'll see

--- build_files/build.sh
+++ build_files/build.sh
@@ -10,13 +10,13 @@ dnf5 copr enable avengemedia/danklinux
 # uses danksearch instead of dsearch as it is not found, idky
 dnf5 install niri dms dgop danksearch -y
 
-systemctl --user enable dms
-systemctl --user add-wants niri.service dms
-
 # should handle all the things needed without needing more setup
 dms greeter install
 # not sure that's needed but we'll see
 dms greeter sync
+
+systemctl enable dms
+systemctl add-wants niri.service dms
 
 # note: could use this build to install all my stuff across systems
 # like zed, zen, bitwarden, localsend, via, etc.
