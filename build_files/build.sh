7e1649d9dc02a81cba0acaf1994f10282036d5de
sixelasacul <pro@sixe.la>
2026-08-02 15:55:42 +0200

    let's try that again

--- build_files/build.sh
+++ build_files/build.sh
@@ -10,10 +10,13 @@ dnf5 copr enable avengemedia/danklinux -y
 # uses danksearch instead of dsearch as it is not found, idky
 dnf5 install niri dms dgop danksearch dms-greeter -y
 
-systemctl enable dms
-systemctl add-wants niri.service dms
-systemctl disable gdm lightdm sddm
-systemctl enable greetd
+# down to this point it works. no idea why but the systemd stuff aren't set.
+# worst case that could be done by the user
+
+systemctl --user enable dms
+systemctl --user add-wants niri.service dms
+systemctl --user disable gdm lightdm sddm
+systemctl --user enable greetd
 
 # note: could use this build to install all my stuff across systems
 # like zed, zen, bitwarden, localsend, via, etc.
