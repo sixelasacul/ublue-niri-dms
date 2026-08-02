4618221e8beccb8437a4a2cfac6a5dcfc5983ba3
sixelasacul <pro@sixe.la>
2026-08-02 15:36:30 +0200

    all semi-manual idk what i'm doing

--- build_files/build.sh
+++ build_files/build.sh
@@ -8,15 +8,12 @@ cp -avf "/ctx/system_files"/. /
 ### Can't use DankInstall from here, so running the installation manually
 dnf5 copr enable avengemedia/danklinux
 # uses danksearch instead of dsearch as it is not found, idky
-dnf5 install niri dms dgop danksearch -y
-
-# should handle all the things needed without needing more setup
-dms greeter install
-# not sure that's needed but we'll see
-dms greeter sync
+dnf5 install niri dms dgop danksearch dms-greeter -y
 
 systemctl enable dms
 systemctl add-wants niri.service dms
+systemctl disable gdm lightdm sddm
+systemctl enable greetd
 
 # note: could use this build to install all my stuff across systems
 # like zed, zen, bitwarden, localsend, via, etc.
