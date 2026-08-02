f5d9cf6df8045e479812cffd92c5df41e1a5add5
sixelasacul <pro@sixe.la>
2026-08-02 11:56:28 +0200

    going for the semi-manual install and see how it goes

--- build_files/build.sh
+++ build_files/build.sh
@@ -5,5 +5,18 @@ set -ouex pipefail
 # Copy the contents of system_files/ of the git repo to /
 cp -avf "/ctx/system_files"/. /
 
-### Easy install of dms and niri with DankInstall
-sudo -v && curl -fsSL https://install.danklinux.com | sh -s -- -c niri -t ghostty --include-deps dms-greeter -y
+### Can't use DankInstall from here, so running the installation manually
+dnf5 copr enable avengemedia/danklinux
+# uses danksearch instead of dsearch as it is not found, idky
+dnf5 install niri dms dgop danksearch
+
+systemctl --user enable dms
+systemctl --user add-wants niri.service dms
+
+# should handle all the things needed without needing more setup
+dms greeter install
+# not sure that's needed but we'll see
+dms greeter sync
+
+# note: could use this build to install all my stuff across systems
+# like zed, zen, bitwarden, localsend, via, etc.
