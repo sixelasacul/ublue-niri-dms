8c4168b6f94644be57bffff54cfeddacfbfb8348
sixelasacul <pro@sixe.la>
2026-08-03 15:19:28 +0200

    oops

--- system_files/usr/lib/shell-setup.sh
+++ system_files/usr/lib/shell-setup.sh
@@ -1,8 +1,11 @@
+#!/bin/bash
+
 systemctl --user enable dms
 systemctl --user add-wants niri.service dms
-
-sudo -v && dms greeter sync -y
 systemctl --user disable gdm lightdm sddm plasmalogin
 systemctl --user enable greetd
 
 cp -r /usr/lib/default-config/niri ~/.config
+
+# normally this uses sudo, not sure how that'd work in a service
+dms greeter sync -y
