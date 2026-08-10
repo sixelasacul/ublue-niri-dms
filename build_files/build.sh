95edba32d9cc4384f8656852050292aa7359364a
sixelasacul <pro@sixe.la>
2026-08-10 20:57:38 +0200

    I think I've messed up something with the copr, changed it to use latest published version of dms

--- build_files/build.sh
+++ build_files/build.sh
@@ -5,19 +5,19 @@ set -ouex pipefail
 # Copy the contents of system_files/ of the git repo to /
 cp -avf "/ctx/system_files"/. /
 
-# Can't use DankInstall from here, so running the installation manually
-dnf5 copr enable avengemedia/danklinux -y
+dnf5 copr enable avengemedia/dms -y
 dnf5 copr enable scottames/ghostty -y
 dnf5 install -y \
     chezmoi \
     ghostty \
     niri \
     dms \
-    dgop \
-    danksearch \
     dms-greeter
+dnf remove xwaylandvideobridge -y
 
-# note: uses danksearch instead of dsearch as it is not found, idky
+# note on packages:
+# - xdg-desktop-portal-gnome is needed for screencasting (especially since the base image is KDE plasma)
+# - chezmoi for customizing the DE and other apps outside of the image
 
 # this is used to run a bunch of systemctl commands once the user logs in for the first time
 ln -s /usr/lib/systemd/system/shell-setup.service /usr/lib/systemd/system/multi-user.target.wants/shell-setup.service
