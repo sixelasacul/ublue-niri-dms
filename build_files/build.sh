4d47110dd5fb9a6a7e36641a40230383d8504fa4
sixelasacul <pro@sixe.la>
2026-08-11 11:16:20 +0200

    forgot portal, and seems like I have to be explicit on optional packages now idk

--- build_files/build.sh
+++ build_files/build.sh
@@ -12,12 +12,19 @@ dnf5 install -y \
     ghostty \
     niri \
     dms \
-    dms-greeter
+    dms-greeter \
+    matugen \
+    cava \
+    dgop \
+    danksearch \
+    cups-pk-helper \
+    xdg-desktop-portal-gnome
 dnf remove xwaylandvideobridge -y
 
 # note on packages:
 # - xdg-desktop-portal-gnome is needed for screencasting (especially since the base image is KDE plasma)
 # - chezmoi for customizing the DE and other apps outside of the image
+# - not installing dankcalendar from here as it can be installed via flatpak and doesn't seem to be stable yet (only dankcalendar-git is available)
 
 # this is used to run a bunch of systemctl commands once the user logs in for the first time
 ln -s /usr/lib/systemd/system/shell-setup.service /usr/lib/systemd/system/multi-user.target.wants/shell-setup.service
